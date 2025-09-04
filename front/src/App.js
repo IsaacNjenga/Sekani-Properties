9cd6ef050c44df7c1d991817837a0befe054264f
Isaac Njenga <ayzzoh20@gmail.com>
2025-09-04 06:00:31 +0300

    first commit

---
+++ front/src/App.js
@@ -0,0 +1,51 @@
+import React, { createContext, useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Properties from "./pages/Properties";
+import { AnimatePresence } from "framer-motion";
+
+export const UserContext = createContext();
+
+export const lightTheme = {
+  backgroundColor: "#f2f5fa",
+  color: "#090c11",
+  secondary: "#85898d",
+  borderColor: "#333",
+};
+
+export const darkTheme = {
+  backgroundColor: "#090c11",
+  color: "#f2f5fa",
+  secondary: "#85898d",
+  borderColor: "#fff",
+};
+
+function App() {
+  const location = useLocation();
+  const [darkMode, setDarkMode] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ darkMode, setDarkMode, isMobile }}>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/properties" element={<Properties />} />
+          </Route>
+        </Routes>
+      </AnimatePresence>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
