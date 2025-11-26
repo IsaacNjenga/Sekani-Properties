92832f9934d0399e89dec6c84f6655925510f13e
Isaac Njenga <ayzzoh20@gmail.com>
2025-11-26 14:18:18 +0300

    na gode

--- front/src/utils/FavouriteFunctions.js
+++ front/src/utils/FavouriteFunctions.js
@@ -1,16 +1,21 @@
+import axios from "axios";
 import { useFavourites } from "../contexts/FavouritesContext";
 
 export function FavouriteFunctions() {
   const { favouriteItems, setFavouriteItems } = useFavourites();
 
   const addToFavourites = (item) => {
+    const url = `${process.env.REACT_APP_API_URL}/analytics/like/${item._id}`;
     setFavouriteItems((prevItems) => [...prevItems, { ...item }]);
+    axios.post(url).catch(() => {});
   };
 
   const removeFromFavourites = (id) => {
+    const url = `${process.env.REACT_APP_API_URL}/analytics/unlike/${id}`;
     setFavouriteItems((prevItems) =>
       prevItems.filter((item) => item._id !== id)
     );
+    axios.post(url).catch(() => {});
   };
 
   const isInFavourites = (item) =>
